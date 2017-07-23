c3127db400c1a7f9515f6a35104feed88cd31163
root <root@ip-172-31-21-90.us-east-2.compute.internal>
2017-07-23 02:19:36 +0000

    finally got the stats how I wanted them to be.

--- compare.sh
+++ compare.sh
@@ -1,8 +1,13 @@
-#make sure that any user is retrievable
-curl -H 'TRN-Api-Key: b4efed97-ffa8-45ee-a16e-6e8d7ed36f76' 'https://pubgtracker.com/api/profile/pc/Autumn0.0'
-#make sure that we are only pulling solo data
-#make sure that we are only pulling data from aggregate region
-#grab kill/death ratio
-#grab skill Rating
-#grab win percentage
+#figuring out the proper command for this script
+#at this point I managed to get the proper query to get the stats for "Solo" and "Aggregate" region
+curl -H 'TRN-Api-Key: b4efed97-ffa8-45ee-a16e-6e8d7ed36f76' 'https://pubgtracker.com/api/profile/pc/Autumn_KR/' | jq '.Stats[]' | jq '. | select ((.Region == "agg") and (.Match == "solo"))'
+#Still need to do
+#----Kill/Death Ratio
+#----Skill Rating
+#----Win %
 
+#Things to consider at this for structured functions
+#----Input two players is the end goal with an option to output the data comparison raw or json?
+#----For further consideration maybe come up with a statistic that filters for stats and/or region?
+#----Function to grab specific stat
+#----Function to output a specific style
